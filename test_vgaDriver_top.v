8c55a93f97f729584dd2adcfffd15dbf648f7413
TheSilentVoice <63007575+TheSilentVoice@users.noreply.github.com>
2023-03-29 18:15:42 +0300

    Modified test for moving ball

--- test_vgaDriver_top.v
+++ test_vgaDriver_top.v
@@ -1,5 +1,5 @@
-
-
+`include "seven_segment.v"
+`include "digit_bitmap.v"
 
 module test_vgaDriver_top(
     input VGA_IN_CLK,
@@ -12,12 +12,14 @@ module test_vgaDriver_top(
 );
 
 
-parameter RED     =16'hF000;
-parameter GREEN   =16'h7E0;
-parameter BLUE    =16'h1F;
-parameter YELLOW  =16'hFFE0;
-parameter MAGENTA =16'hF81F;
-parameter CYAN    =16'h7FF;
+parameter RED     = 16'hF000;
+parameter GREEN   = 16'h7E0;
+parameter BLUE    = 16'h1F;
+parameter YELLOW  = 16'hFFE0;
+parameter MAGENTA = 16'hF81F;
+parameter CYAN    = 16'h7FF;
+parameter BLACK   = 16'h0;
+parameter WHITE   = 16'hFFFF;
 
 
 wire [9:0] vpos;
@@ -37,24 +39,93 @@ always@(posedge VGA_IN_CLK) begin
     end
 end
 
-always@(hpos || vpos ) begin
-    if((hpos&7)==0 || (vpos&7)==0) begin
-        VGA_RGB_IN = RED;
+//PATTERN GENERATOR
+//--------------------------------------------------------------------
+//always@(hpos || vpos) begin
+//    if((hpos&7)==0|| (vpos&7)==0) begin
+//        VGA_RGB_IN = RED;
+//    end 
+//    else begin
+//        if(hpos[4]) begin
+//            VGA_RGB_IN = GREEN;
+//        end 
+//        else begin
+//            if(vpos[4]) begin
+//                VGA_RGB_IN = BLUE;
+//            end 
+//            else begin
+//                VGA_RGB_IN = BLACK;
+//            end
+//        end
+//    end
+//end
+//--------------------------------------------------------------------
+
+
+reg signed [8:0] ball_hpos;
+reg signed [8:0] ball_vpos;
+
+reg signed [8:0] ball_horiz_move = -2;
+reg signed [8:0] ball_vert_move = 2;
+
+localparam hinit = 128;
+localparam vinit = 128;
+localparam size = 4;
+
+wire [8:0] ball_hdiff = hpos - ball_hpos;
+wire [8:0] ball_vdiff = vpos - ball_vpos;
+
+wire ball_hgfx = ball_hpos == hpos;
+wire ball_vgfx = ball_vpos == vpos;
+wire ball_gfx = ball_hgfx && ball_vgfx;
+
+
+
+
+
+always @(posedge VGA_VSYNC or posedge reset) begin
+    if (reset) begin
+        ball_hpos <= hinit;
+        ball_vpos <= vinit;
     end 
     else begin
-        if(vpos[4]) begin
-            VGA_RGB_IN = GREEN;
-        end 
-        else begin
-            if(hpos[4]) begin
-                VGA_RGB_IN = BLUE;
-            end
-        end
+        ball_hpos <= ball_hpos + ball_horiz_move;
+        ball_vpos <= ball_vpos + ball_vert_move;
     end
 end
 
 
 
+
+wire ball_horiz_collide = ball_hpos >= 640;
+wire ball_vert_collide  = ball_vpos >= 480;
+
+always @(posedge ball_vert_collide) begin
+    ball_vert_move <= -ball_vert_move;
+end
+
+always @(posedge ball_horiz_collide) begin
+    ball_horiz_move <= -ball_horiz_move;
+end
+
+
+always@(*) begin
+    if (ball_hgfx | ball_gfx) begin
+        VGA_RGB_IN <= BLUE;
+    end
+    else begin
+        if (ball_vgfx | ball_gfx) begin
+            VGA_RGB_IN <= RED;
+        end
+        else begin
+            VGA_RGB_IN <= BLACK;
+        end
+    end
+
+    if(ball_gfx)
+        VGA_RGB_IN <= WHITE;
+end
+    
 
 vgaDriver driver(
     .clk_i(VGA_IN_CLK),
